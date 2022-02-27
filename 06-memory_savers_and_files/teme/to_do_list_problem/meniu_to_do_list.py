e0365e144f259d74c4bc5c6d2f872ea6434cfd0d
unknown <bogdan_cilica@yahoo.com>
2022-02-27 21:58:28 +0200

    tema fisiere 75%

--- 06-memory_savers_and_files/teme/to_do_list_problem/meniu_to_do_list.py
+++ 06-memory_savers_and_files/teme/to_do_list_problem/meniu_to_do_list.py
@@ -32,6 +32,8 @@ import csv
 import pandas as pd
 
 lista_taskuri = pd.read_csv('taskuri.csv', index_col=0)
+print("Lista initiala")
+print(lista_taskuri)
 
 # creare meniu
 print("""
@@ -47,22 +49,43 @@ while meniu not in "123456":
     meniu = input("Nu ati ales din lista. Mai incercati (1, 2, 3, 4, 5, 6): ")
 #
 if meniu == "1":
-    print(lista_taskuri)
+    print(lista_taskuri.sort_values("Categorie"))
+
+if meniu == "2":
+    print("""
+1. sortare ascendentă task
+2. sortare descendentă task
+3. sortare ascendentă data
+4. sortare descendentă data
+5. sortare ascendentă persoana responsabilă
+6. sortare descendentă persoană responsabilă
+7. sortare ascendentă categorie
+8. sortare descendentă categorie
+    """)
+    meniu_2 = input("Ati ales optiunea 2. Alegeti sub-optiunea din lista de mai sus (1, 2, 3, 4, 5, 6, 7, 8): ")
+    while meniu_2 not in "12345678":
+        meniu_2 = input("Nu ati ales din lista. Mai incercati (1, 2, 3, 4, 5, 6, 7, 8): ")
+    if meniu_2 == "1":
+        print(lista_taskuri.sort_values("Nume Task"))
+    elif meniu_2 == "2":
+        print(lista_taskuri.sort_values("Nume Task", ascending=False))
+# TODO: shimba formatul la data yyyy.mm.dd pentru ordonare corecta
+    elif meniu_2 == "3":
+        print(lista_taskuri.sort_values("Data Limita"))
+    elif meniu_2 == "4":
+        print(lista_taskuri.sort_values("Data Limita", ascending=False))
+    elif meniu_2 == "5":
+        print(lista_taskuri.sort_values("Responsabil"))
+    elif meniu_2 == "6":
+        print(lista_taskuri.sort_values("Responsabil", ascending=False))
+    elif meniu_2 == "7":
+        print(lista_taskuri.sort_values("Categorie"))
+    else:
+        print(lista_taskuri.sort_values("Categorie", ascending=False))
+
+#TODO: "3. Filtrare date 4. Adaugare task 5. Editare task 6. Stergere task"
 
 
-#TODO: "2. Sortare 3. Filtrare date 4. Adaugare task 5. Editare task 6. Stergere task"
-
-# if meniu == "2":
-#     print("""
-#     # 1. sortare ascendentă task
-#     # 2. sortare descendentă task
-#     # 3. sortare ascendentă data
-#     # 4. sortare descendentă data
-#     # 5. sortare ascendentă persoana responsabilă
-#     # 6. sortare descendentă persoană responsabilă
-#     # 7. sortare ascendentă categorie
-#     # 8. sortare descendentă categorie
-#     """)
-#     meniu_2 = input("Ati ales optiunea 2. Alegeti sub-optiunea din lista de mai sus (1, 2, 3, 4, 5, 6, 7, 8): ")
-#     while meniu_2 not in "12345678":
-#         meniu_2 = input("Nu ati ales din lista. Mai incercati (1, 2, 3, 4, 5, 6, 7, 8): ")
+# 5 Adaugare task
+# lista_taskuri.iloc[x,y] # for specific location
+# lista_taskuri.loc[lista_taskuri["Nume task"] == "Bogdan"] - printeaza doar taskurile lui Bogdan
