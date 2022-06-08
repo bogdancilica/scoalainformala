8fec405b1394ae5c363391b684cb91145023a4fe
unknown <bogdan_cilica@yahoo.com>
2022-06-08 20:08:59 +0300

    'app7'

--- manufacturing_inventory/aplicatie_reports/views.py
+++ manufacturing_inventory/aplicatie_reports/views.py
@@ -1,6 +1,7 @@
 
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.forms.utils import ErrorList
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -23,20 +24,33 @@ class GetItemView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     fields = '__all__'
     template_name = 'aplicatie_reports/report_form.html'
 
+
     def test_func(self):
         if self.request.user.is_superuser:
             return True
         return False
 
-    def get_success_url(self):
-        instance_report = self.model.objects.get(id=self.object.id)
-        quantity_to_diff = instance_report.quantity
-        item_instance = Items.objects.get(id=instance_report.item_no.id)
-        initial_quantity = item_instance.quantity
-        if initial_quantity >= quantity_to_diff:
+
+    def form_valid(self, form):
+        # if form.is_valid():
+        # print(form.cleaned_data['item_no'])
+        if form.cleaned_data['quantity'] >= Items.objects.get(item_no=form.cleaned_data['item_no']).quantity:
+            errors = form._errors.setdefault('quantity', ErrorList())
+            errors.append('Cantitatea este insuficienta!')
+            return super(GetItemView, self).form_invalid(form)
+        else:
+            instance = form.save()
+            instance_report = self.model.objects.get(id=instance.id)
+            quantity_to_diff = instance_report.quantity
+            item_instance = Items.objects.get(id=instance_report.item_no.id)
+            initial_quantity = item_instance.quantity
             item_instance.quantity = initial_quantity - quantity_to_diff
             item_instance.save()
-            return reverse('reports:reports_list')
+        return super(GetItemView, self).form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('reports:reports_list')
 
 
 def report_user(request):
